6927c9241c03418744132f45e1b90bef0dde5fd3
Shay Dwight <shay.dwight+create@gmail.com>
2017-07-26 17:32:00 -0600

    Create jquery_functions.js

---
+++ javascripts/jquery_functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#PowerOff").click(function(){
+        $("#game").fadeOut(1000);
+    });
+    $("#PowerOn").click(function(){
+        $("#game").fadeIn(1000);
+    });
+});
